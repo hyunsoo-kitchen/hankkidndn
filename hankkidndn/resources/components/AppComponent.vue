c197ec31fd74cd117f663ec3f008f9c6610851e4
m1nute <simple_0714@naver.com>
2024-06-05 17:42:21 +0900

    240605 17:42 노경호

--- hankkidndn/resources/components/AppComponent.vue
+++ hankkidndn/resources/components/AppComponent.vue
@@ -1,11 +1,40 @@
 <template>
-    <div>
-        <router-view></router-view>
+    <!-- Header -->
+    <header>
+    <div class="header-container">
+      <div class="header-content">
+        <img class="img-logo" alt="logo" src="/logo.png">
+        <div class="title">
+          <router-link to="/board"><h1>한끼든든</h1></router-link>
+        </div>
+        <div class="btn-group">
+          <div v-if="!$store.state.authFlg">
+            <button @click="$router.push('/login')" class="">로그인</button>
+            <button @click="$router.push('/registration')" class="">가입하기</button>
+          </div>
+          <div v-else>
+            <button @click="$store.dispatch('logout')" class="btn btn-header btn-bg-black">로그아웃</button>
+          </div>
+        </div>
+      </div>
     </div>
+  </header>
+
+    <!-- Main -->
+    <main>
+        <router-view></router-view>
+
+    </main>
+
+    <!-- Footer -->
+    <footer>
+
+
+    </footer>
 </template>
 <script setup>
 
 </script>
 <style>
-    
+    @import url('../css/common.css');
 </style>
