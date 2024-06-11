33d1cb11d0f6704cfdef6f6c99c940f2b09f4e5a
m1nute <simple_0714@naver.com>
2024-06-11 18:09:53 +0900

    0611 노경호

--- doc/NGH/JavaScript/test2.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('mobile-menu').addEventListener('click', function() {
-    const nav = document.querySelector('nav ul');
-    nav.classList.toggle('showing');
-});
--- hankkidndn/resources/components/AppComponent.vue
+++ hankkidndn/resources/components/AppComponent.vue
@@ -8,16 +8,16 @@
               <router-link to="/main"><h1>한끼든든</h1></router-link>
             </div>
                 <div class="btn-group" v-if="!$store.state.authFlg">
-                  <button @click="$router.push('/login')" class="header-btn">로그인</button>
+                  <button @click="$router.push('/login')" class="header-btn" id="login">로그인</button>
                 </div>
                 <div class="btn-group" v-if="!$store.state.authFlg">
-                  <button @click="$router.push('/registration')" class="header-btn">가입하기</button>
+                  <button @click="$router.push('/registration')" class="header-btn" id="regist">가입하기</button>
                 </div>
                 <div class="btn-group" v-else>
                   <button @click="$store.dispatch('logout')" class="header-btn myInfo"></button>
                 </div>
                 <div class="btn-group" v-else>
-                  <button @click="$store.dispatch('logout')" class="header-btn">로그아웃</button>
+                  <button @click="$store.dispatch('logout')" class="header-btn" id="logout">로그아웃</button>
                 </div>
           </div>
         </div>
@@ -31,6 +31,47 @@
     </footer>
 </template>
 <script setup>
+import { onMounted, onBeforeUnmount } from 'vue';
+
+function displayImage() {
+  const loginImage = document.querySelector('#login');
+  const registImage = document.querySelector('#regist');
+  loginImage.style.display = 'block';
+  registImage.style.display = 'block';
+  loginImage.textContent = "";
+  registImage.textContent = "";
+  loginImage.innerHTML = '<img src="/login.png" alt="login">';
+  registImage.innerHTML = '<img src="/regist.png" alt="register">';
+}
+function closeImage() {
+  const loginImage = document.querySelector('#login');
+  const registImage = document.querySelector('#regist');
+  loginImage.style.display = 'block';
+  registImage.style.display = 'block';
+  loginImage.textContent = "로그인";
+  registImage.textContent = "가입하기";
+}
+
+// 화면 크기 변경을 감지하는 함수
+function handleResize() {
+  if (window.innerWidth <= 1044) {
+    displayImage();
+  } else {
+    closeImage();
+  }
+}
+
+// 컴포넌트가 마운트될 때 이벤트 리스너를 추가하고 초기 상태를 설정
+onMounted(() => {
+  window.addEventListener('resize', handleResize);
+  handleResize();
+});
+
+// 컴포넌트가 언마운트될 때 이벤트 리스너를 제거
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', handleResize);
+});
+
 </script>
 <style>
     @import url('../css/common.css');
