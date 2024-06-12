092f5d01463c153824871b9f8baf0d23aad7ac51
jubiber <myemail@example.com>
2024-06-12 21:50:29 +0900

    240612 join.js study

--- doc/JEH/js/join.js
+++ doc/JEH/js/join.js
@@ -1,5 +1,5 @@
      // 휴대전화 국가 코드
-     const phoneCodeSelect = document.getElementById("phone-code");
+     const phoneCodeSelect = document.querySelector("#phone-code");
 
      // 휴대전화 국가 코드 선택 시 이벤트 핸들러
      phoneCodeSelect.addEventListener("change", function() {
@@ -7,8 +7,8 @@
          const selectedCode = phoneCodeSelect.value;
          
          // 국가 코드에 따라 휴대전화 입력란의 placeholder 변경
-         const phone2Input = document.getElementById("phone2");
-         const phone3Input = document.getElementById("phone3");
+         const phone2Input = document.querySelector("#phone2");
+         const phone3Input = document.querySelector("#phone3");
          
          switch(selectedCode) {
              case "010":
@@ -23,47 +23,59 @@
                  phone2Input.placeholder = "4567";
                  phone3Input.placeholder = "8901";
                  break;
-             // 필요한 국가 코드에 대한 case 추가
              default:
                  break;
          }
  });
  function execDaumPostcode() {
+    // 새로운 Daum 우편번호 찾기 객체를 생성하고, 완료 시 호출될 콜백 함수를 설정합니다.
     new daum.Postcode({
+        // 우편번호 찾기가 완료되었을 때 실행될 콜백 함수입니다.
         oncomplete: function(data) {
-            // 팝업을 통한 검색 결과 항목 클릭 시 실행
-            var addr = ''; // 주소_결과값이 없을 경우 공백 
-            var extraAddr = ''; // 참고항목
+            var addr = ''; // 주소 정보를 저장할 변수를 초기화합니다.
+            var extraAddr = ''; // 추가 주소 정보를 저장할 변수를 초기화합니다.
 
-            //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
-            if (data.userSelectedType === 'R') { // 도로명 주소를 선택
-                addr = data.roadAddress;
-            } else { // 지번 주소를 선택
-                addr = data.jibunAddress;
+            // 사용자가 선택한 주소의 타입이 도로명 주소인 경우
+            if (data.userSelectedType === 'R') { 
+                addr = data.roadAddress; // 도로명 주소를 addr 변수에 저장합니다.
+            } else { 
+                addr = data.jibunAddress; // 지번 주소를 addr 변수에 저장합니다.
             }
-
+            // 사용자가 선택한 주소의 타입이 도로명 주소인 경우
             if(data.userSelectedType === 'R'){
+                // 법정동명이 존재하고, 동, 로, 가로 끝나는 경우 추가 주소 정보를 설정합니다.
                 if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
                     extraAddr += data.bname;
                 }
+                // 건물명이 존재하고, 아파트인 경우 추가 주소 정보를 설정합니다.
                 if(data.buildingName !== '' && data.apartment === 'Y'){
                     extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
                 }
+                // 추가 주소 정보가 존재하는 경우 괄호로 묶어서 설정합니다.
                 if(extraAddr !== ''){
                     extraAddr = ' (' + extraAddr + ')';
                 }
             } else {
-                document.getElementById("UserAdd1").value = '';
+                 // 사용자가 선택한 주소의 타입이 지번 주소인 경우 추가 주소 정보를 비웁니다.
+                document.querySelector("#UserAdd1").value = '';
             }
-
-            // 선택된 우편번호와 주소 정보를 input 박스에 넣는다.
-            document.getElementById('zipp_code_id').value = data.zonecode;
-            document.getElementById("UserAdd1").value = addr;
-            document.getElementById("UserAdd1").value += extraAddr;
-            document.getElementById("UserAdd2").focus(); // 우편번호 + 주소 입력이 완료되었음으로 상세주소로 포커스 이동
+            // 우편번호 입력 필드에 우편번호를 설정
+            document.querySelector('#zipp_code_id').value = data.zonecode;
+            // 기본주소 영역
+            document.querySelector("#UserAdd1").value = addr;
+            // 추가 주소정보 입력
+            document.querySelector("#UserAdd1").value += extraAddr;
+            // 두 번째 주소 입력 필드에 포커스를 설정합니다.
+            document.querySelector("#UserAdd2").focus(); 
         }
+    // 우편번호 찾기 창을 엽니다.
     }).open();
-    document.getElementById('phone2').addEventListener('input', function (e) {
+
+    // 휴대폰번호 숫자만
+    document.querySelector('#phone2').addEventListener('input', function (e) {
+        this.value = this.value.replace(/[^0-9]/g, '');
+    });
+    document.querySelector('#phone3').addEventListener('input', function (e) {
         this.value = this.value.replace(/[^0-9]/g, '');
     });
     
