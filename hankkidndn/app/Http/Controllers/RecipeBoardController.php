88d074b46df42f84aba352b889a0db4da8991f58
breadhouse1 <holiclong@naver.com>
2024-06-13 17:59:37 +0900

    240613 권현수 카테고리 작업

--- hankkidndn/app/Http/Controllers/RecipeBoardController.php
+++ hankkidndn/app/Http/Controllers/RecipeBoardController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Recipe_boards;
 use Illuminate\Http\Request;
 
 class RecipeBoardController extends Controller
 {
-    //
+    // 각 페이지의 리스트 획득
+    public function getList($num) {
+        if($num === 100) {
+            $recipeData = Recipe_boards::select('recipe_boards.*', 'users.u_nickname')
+                                ->join('users', 'users.id', '=', 'recipe_boards.user_id')
+                                ->orderBy('recipe_boards.id', 'DESC')
+                                ->limit(16)
+                                ->get();
+        } else if($num === 99 ) {
+            $recipeData = Recipe_boards::select('recipe_boards.*', 'users.u_nickname')
+                                ->join('users', 'users.id', '=', 'recipe_boards.user_id')
+                                ->orderBy('recipe_boards.likes_num', 'ESC')
+                                ->limit(16)
+                                ->get();
+        } else {
+            $recipeData = Recipe_boards::select('recipe_boards.*', 'users.u_nickname')
+                                ->where('boards_type_id', '=', $num)
+                                ->limit(16)                        
+                                ->get();
+        }
+        
+        $responseData = [
+            'code' => '0'
+            ,'msg' => '추가게시글 획득 완료'
+            ,'data' => $recipeData->toArray()
+        ];
+
+        return response()->json($responseData, 200);
+    }
 }
--- hankkidndn/resources/js/store.js
+++ hankkidndn/resources/js/store.js
@@ -1,16 +1,48 @@
 import { createStore } from 'vuex';
+import router from './router';
 
 const store = createStore({
     state() {
         return {
-
+            recipeListData: [],
+            BoardListData:[],
+            authFlg: document.cookie.indexOf('auth=') >= 0 ? true : false,
         }
     },
     mutations: {
-
+        setRecipeBoardData(state, data) {
+            state.recipeListData = data;
+        },
+        setBoardData(state, data) {
+            state.BoardListData = data;
+        }
     },
     actions: {
+        // 보드 페이지 이동
+        getRecipeList(context, num) {
+            const url = '/recipe=' + num;
 
+            axios.get(url)
+            .then(response => {
+                console.log(response.data.data)
+                context.commit('setRecipeBoardData', response.data.data);
+
+                router.push('/recipe=' + num);
+            })
+            .catch()
+        },
+
+        getBoardList(context, num) {
+            const url = '/board=' + num;
+
+            axios.get(url)
+            .then(response => {
+                console.log(response.data.data)
+                context.commit('setBoardData', response.data.data);
+                router.push('/board='+ num);
+            })
+            .catch()
+        }
     }
 });
 
