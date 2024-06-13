c4a98a2f33baccc18dcfe856f4a197fea8df53fb
m1nute <simple_0714@naver.com>
2024-06-13 10:34:07 +0900

    0613 노경호

--- hankkidndn/resources/components/MainComponent.vue
+++ hankkidndn/resources/components/MainComponent.vue
@@ -4,30 +4,30 @@
         <div class="nav-container">
             <div @click="openModal" id="hambuger" :style="hambuger"></div>
             <div @click="closeModal" id="close-hambuger" :style="closeHambuger"></div>
-            <nav v-if="!isMobileView" id="nav-list">
-                <ul>
-                    <li><a href="#">추천</a></li>
-                    <li class="dropdown">
-                        <a href="#">레시피</a>
-                        <ul class="dropdown-content">
-                            <li><a @click="$router.push('/recipelist')">한식</a></li>
-                            <li><a href="#">중식</a></li>
-                            <li><a href="#">양식</a></li>
-                            <li><a href="#">일식</a></li>
-                            <li><a href="#">베이킹</a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#">게시판</a>
-                        <ul class="dropdown-content">
-                            <li><a href="#">공지사항</a></li>
-                            <li><a href="#">자유게시판</a></li>
-                            <li><a href="#">질문게시판</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="#">이벤트</a></li>
-                </ul>
-            </nav>
+                <nav v-if="!isMobileView" id="nav-list">
+                    <ul>
+                        <li><button>추천</button></li>
+                        <li class="dropdown">
+                            <button>레시피</button>
+                            <ul class="dropdown-content">
+                                <li><button @click="$router.push('/recipelist')">한식</button></li>
+                                <li><button @click="$router.push('/recipelist')">중식</button></li>
+                                <li><button @click="$router.push('/recipelist')">양식</button></li>
+                                <li><button @click="$router.push('/recipelist')">일식</button></li>
+                                <li><button @click="$router.push('/recipelist')">베이킹</button></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <button>게시판</button>
+                            <ul class="dropdown-content">
+                                <li><button>공지사항</button></li>
+                                <li><button>자유게시판</button></li>
+                                <li><button>질문게시판</button></li>
+                            </ul>
+                        </li>
+                        <li><button>이벤트</button></li>
+                    </ul>
+                </nav>
         </div>
         <nav v-if="isModalOpen" class="hambuger-nav-list">
             <ul class="dropdown-content">
