fd9238096a73dd12fbe1f7f90e6e499d42027c01
m1nute <simple_0714@naver.com>
2024-06-13 14:53:36 +0900

    0613웰컴 노경호

--- hankkidndn/resources/views/welcome.blade.php
+++ hankkidndn/resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
     {{-- 헤더안에 csrf 토큰생성 --}}
     <meta name="csrf-token" content="{{ csrf_token() }}"> 
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js" defer></script>
     <title>Hankkidndn</title>
 </head>
 <body>
