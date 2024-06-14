355f2450051e9691194b90f3e9d58b4b463731b9
m1nute <simple_0714@naver.com>
2024-06-14 11:38:53 +0900

    0614 노경호 회원가입 처리

--- hankkidndn/resources/js/store.js
+++ hankkidndn/resources/js/store.js
@@ -4,9 +4,12 @@ import router from './router';
 const store = createStore({
     state() {
         return {
+            // 인증 flg
+            authFlg: document.cookie.indexOf('auth=') >= 0 ? true : false,
+            // 유저 정보 받아오는 쪽
+            userInfo: localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null,
             recipeListData: [],
             BoardListData:[],
-            authFlg: document.cookie.indexOf('auth=') >= 0 ? true : false,
         }
     },
     mutations: {
@@ -16,6 +19,15 @@ const store = createStore({
         setBoardData(state, data) {
             state.BoardListData = data;
         }
+        ,
+        // 인증 플래그 저장
+        setAuthFlg(state, flg) {
+            state.authFlg = flg;
+        },
+        // 유저 정보 저장
+        setUserInfo(state, data) {
+            state.boardData = data;
+        },
     },
     actions: {
         // 보드 페이지 이동
@@ -42,24 +54,25 @@ const store = createStore({
                 router.push('/board='+ num);
             })
             .catch()
+        },
+        
+        //회원가입 처리 action
+        registration(context) {
+            const url = '/api/registration';
+            const data = new FormData(document.querySelector('#registrationForm'));
+
+            axios.post(url, data)
+            .then(response => {
+                console.log(response.data) //TODO
+                router.replace('/login');
+            })
+            .catch(error => {
+                console.log(error.response.data); //TODO
+                alert('회원가입에 실패했습니다 (' + error.response.data.code + ')');
+            });
         }
     }
 });
 
 export default store;
 
-// 회원가입 처리 action
-// registration(context) {
-//     const url = '/api/registration';
-//     const data = new FormData(document.querySelector('#registForm'));
-
-//     axios.post(url, data)
-//     .then(response => {
-//         console.log(response.data) //TODO
-//         router.replace('/login');
-//     })
-//     .catch(error => {
-//         console.log(error.response.data); //TODO
-//         alert('회원가입에 실패했습니다 (' + error.response.data.code + ')');
-//     });
-// }
