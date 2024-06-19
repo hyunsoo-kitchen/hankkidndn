cf832203e4b2293d4f8935ca1af2316be17cff69
haem1n2 <uehek1234@gmail.com>
2024-06-19 18:08:52 +0900

    240619 이현수 css수정 나머지 수정

--- hankkidndn/resources/components/BoardPage/BoardDetailComponent.vue
+++ hankkidndn/resources/components/BoardPage/BoardDetailComponent.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="container">
          <!-- 삭제 모달 -->
-        <div v-if="modalFlg" class="delete-modal">
-            <div class="modal-title">정말로 삭제 하시겠습니까?</div>
-            <div class="delete-btn">
-                <button @click="$store.dispatch('boardDelete', id)">삭제</button>
-                <button @click="closeModal()">취소</button>
-            </div>
+    <div v-if="modalFlg" class="delete-modal">
+        <div class="modal-title">정말로 삭제 하시겠습니까?</div>
+        <div class="delete-btn">
+        <button @click="$store.dispatch('boardDelete', id)">삭제</button>
+        <button @click="closeModal()">취소</button>
         </div>
+    </div>
         <div class="main_list">
             <div class="main_title">
                 <h2 class="title_name">작성게시판</h2>
@@ -25,27 +25,29 @@
                 </div>
                 <hr>
                 <div class="main_content">
+                    <div class="image-container">
                     <img v-for="(item, index) in $store.state.boardImg" :key="index" :src="item.img_path">
+                    </div>
                     <p>{{ $store.state.boardDetail.content }}</p>
                 </div>
                 <div class="comment-section">
                     <div class="comment">
-                        <div class="comment-header">
-                            <p class="comment-author">이현수수깡</p>
-                            <p class="comment-date">2024-06-19 13:00:02</p>
-                        </div>
-                        <p class="comment-content">ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ 감사합니다</p>
-                        <div class="comment-actions">
-                            <button class="like-button"><img src="../../../../hankkidndn/public/img/like.png"></button>
-                            <p>60</p>
-                        </div>
-                    </div>
-                    <div class="comment-form">
-                        <input type="text" placeholder="댓글" class="comment-input"/>
-                        <button class="comment-submit">댓글</button>
-                    </div>
+                    <div class="comment-header">
+                    <p class="comment-author">이현수수깡</p>
+                    <p class="comment-date">2024-06-19 13:00:02</p>
+                </div>
+                <p class="comment-content">ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ 감사합니다</p>
+                <div class="comment-actions">
+                <button class="like-button"><img src="../../../../hankkidndn/public/img/like.png"></button>
+                <p>60</p>
                 </div>
             </div>
+            <div class="comment-form">
+            <input type="text" placeholder="댓글" class="comment-input"/>
+            <button class="comment-submit">댓글</button>
+            </div>
+        </div>
+    </div>
         </div>
     </div>
 </template>
@@ -75,6 +77,33 @@ function closeModal() {
 onBeforeMount(() => {
     store.dispatch('getBoardDetail', data.id);
 });
+// import { mapState } from 'vuex';
+
+// export default {
+//     computed: {
+//         ...mapState({
+//             boardDetail: state => state.boardDetail
+//         })
+//     },
+//     methods: {
+//         goToEditPage() {
+//             const boardId = this.$route.params.id;
+//             this.$router.push(`/board/edit/${boardId}`);
+//         },
+//         deleteBoard() {
+//             const boardId = this.$route.params.id;
+//             this.$store.dispatch('deleteBoard', boardId)
+//                 .then(() => {
+//                     alert('게시글이 삭제되었습니다.');
+//                     this.$router.push('/board');
+//                 })
+//                 .catch(error => {
+//                     console.log(error.response.data);
+//                     alert('게시글 삭제에 실패했습니다. (' + error.response.data.code + ')');
+//                 });
+//         }
+//     }
+// };$store.dispatch('getBoardDetail', item.id)
 </script>
 <style scoped src="../../css/boarddetail.css">
     @import url('https://fonts.googleapis.com/css2?family=Jua&display=swap');
--- hankkidndn/resources/components/RecipePage/RecipeDetailComponent.vue
+++ hankkidndn/resources/components/RecipePage/RecipeDetailComponent.vue
@@ -3,39 +3,30 @@
         <div class="header">
             <div class="header-img-wrapper">
                 <img class="header-img" src="" alt="">
-                <button class="header-like">하트</button>
+                <button class="header-like"><img src="../../../public/img/like.png" alt=""></button>
                 <div class="header-view">뷰123</div>
             </div>
             <div class="header-userinfo">
                 <img class="header-profile" src="" alt="">
-                <div class="header-name">닉네임</div>
+                <div class="header-name">{{ $store.state.recipeDetail.nickname }}</div>
             </div>
         </div>
         <div class="explain">
-            <div class="explain-title">제목</div>
-            <div>설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명</div>
+            <div class="explain-title">{{ $store.state.recipeDetail.title }}</div>
+            <div>{{ $store.state.recipeDetail.content }}</div>
         </div>
         <div class="stuff">
             <div class="stuff-title">재료</div>
             <div class="stuff-all">
-                <div class="stuff-name">재료 이름 : </div>
-                <div class="stuff-gram">재료 양 : </div>
-            </div>
-            <div class="stuff-all">
-                <div class="stuff-name">재료 이름 : </div>
-                <div class="stuff-gram">재료 양 : </div>
+                <div class="stuff-name">{{ $store.state.recipeDetail.stuff }} : </div>
+                <div class="stuff-gram">{{ $store.state.recipeDetail.stuff_gram }} : </div>
             </div>
         </div>
         <div class="line"></div>
         <div class="recipe">
-            <img class="recipe-img" src="" alt="">
+            <img class="recipe-img" :src="$store.state.recipeDetail.img_path" alt="">
             <div class="recipe-order">순서1</div>
-            <div class="recipe-content">설명</div>
-        </div>
-        <div class="recipe">
-            <img class="recipe-img" src="" alt="">
-            <div class="recipe-order">순서1</div>
-            <div class="recipe-content">설명</div>
+            <div class="recipe-content">{{ $store.state.recipeDetail.program_content }}</div>
         </div>
         <div class="video">
             <div class="video-title">동영상</div>
@@ -45,65 +36,13 @@
         <div class="profile">
             <div class="profile-title">레시피 작성자</div>
             <img src="" alt="" class="profile-img">
-            <div class="profile-name">이현수달</div>
-            <div class="profile-link">마이페이지링크</div>
+            <div class="profile-name">{{ $store.state.recipeDetail.profile }}</div>
+            <div class="profile-link">{{ $store.state.recipeDetail.video_link }}</div>
         </div>
         <div class="comments">
             <div class="comments-title">요리후기 : 댓글수</div>
             <div class="line"></div>
             <div class="comment-container">
-                <div class="comments-detail">
-                    <img class="comment-img" src="" alt="">
-                    <span class="comment-name">홍길동</span>
-                    <span class="comment-date">2024-06-11 02:33</span>
-                    <div class="comment-content">댓글내용</div>
-                    <div class="comment-retouch">
-                        <button>수정</button>
-                        <button>삭제</button>
-                    </div>
-                    <div class="comment-reply">
-                        <button class="comment-reply-btn">답글</button>
-                    </div>
-                </div>
-                <div class="comments-detail">
-                    <img class="comment-img" src="" alt="">
-                    <span class="comment-name">홍길동</span>
-                    <span class="comment-date">2024-06-11 02:33</span>
-                    <div class="comment-content">댓글내용</div>
-                    <div class="comment-retouch">
-                        <button>수정</button>
-                        <button>삭제</button>
-                    </div>
-                    <div class="comment-reply">
-                        <button class="comment-reply-btn">답글</button>
-                    </div>
-                </div>
-                <div class="comments-detail">
-                    <img class="comment-img" src="" alt="">
-                    <span class="comment-name">홍길동</span>
-                    <span class="comment-date">2024-06-11 02:33</span>
-                    <div class="comment-content">댓글내용</div>
-                    <div class="comment-retouch">
-                        <button>수정</button>
-                        <button>삭제</button>
-                    </div>
-                    <div class="comment-reply">
-                        <button class="comment-reply-btn">답글</button>
-                    </div>
-                </div>
-                <div class="comments-detail">
-                    <img class="comment-img" src="" alt="">
-                    <span class="comment-name">홍길동</span>
-                    <span class="comment-date">2024-06-11 02:33</span>
-                    <div class="comment-content">댓글내용</div>
-                    <div class="comment-retouch">
-                        <button>수정</button>
-                        <button>삭제</button>
-                    </div>
-                    <div class="comment-reply">
-                        <button class="comment-reply-btn">답글</button>
-                    </div>
-                </div>
                 <div class="comments-detail">
                     <img class="comment-img" src="" alt="">
                     <span class="comment-name">홍길동</span>
@@ -126,7 +65,7 @@
     </div>
 </template>
 <script setup>
-
+import { useStore } from 'vuex';
 </script>
 <style scoped src="../../css/recipedetail.css">
     @import url('https://fonts.googleapis.com/css2?family=Jua&display=swap');
